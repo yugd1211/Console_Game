fc22752ffc379c50ca99622e372492e6505f2455
yugd1211 <yugd1211@gmail.com>
2024-09-30 02:35:22 +0900

    FEAT : 전방 이외에 양옆이 벽 일때 + 왼쪽 벽면에 통로가 있는 경우 이에 맞는 출력 구현

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
 #include <crtdbg.h>
+#include <cmath>
 #include <windows.h>
 
 #define MAP_SIZE 72
@@ -17,6 +18,16 @@
 #define PATH_SIZE MOVE_STEP_SIZE * 2
 const static string Void = "  ";
 const static string Wall = "■";
+
+int GetPlayerPoston(const vector<vector<int>>& playerMap)
+{
+	for (int i = 0; i < playerMap.size(); i++)
+	{
+		if (playerMap[i][1] == 2)
+			return i;
+	}
+	return -1;
+}
 
 void DisplayBoard(vector<vector<string>> &str)
 {
@@ -65,84 +76,138 @@ void SetOutLine(vector<vector<string>>& str, int in_line)
 	}
 }
 
-void SetOutLineLeft(vector<vector<string>>& displayBoard, int in_line, int num, const vector<vector<int>>& playerMap)
+
+void SetLeftEdgeWall(vector<vector<string>>& board, int in_line, int num)
 {
+	int out_line = in_line + num;
+	for (int i = in_line; i < out_line; i++)
+	{
+		if (i < 0 || i >= MAP_SIZE / 2 - 2)
+			continue;
+		board[i][i] = Wall;
+		board[MAP_SIZE - i - 1][i] = Wall;
+	}
+}
+
+void SetLeftHorizontalWall(vector<vector<string>>& board, int in_line, int num)
+{
+	int out_line = in_line + num + 2;
+	for (int i = 0; i < num; i++)
+	{
+		if (in_line + i >= MAP_SIZE / 2)
+			continue;
+		if (in_line + num > MAP_SIZE / 2)
+			continue;
+		board[in_line + num][in_line + i] = Void;
+		board[MAP_SIZE - in_line - num - 1][in_line + i] = Void;
+	}
+}
+
+void SetLeftVerticalWall(vector<vector<string>>& board, int in_line, int num, bool isOutline)
+{
+
+	//왼쪽에서 코너까지의 벽
+	if (in_line >= 0)
+	{
+		for (int i = in_line; i < MAP_SIZE - in_line ; i++)
+			board[i][in_line] = Void;
+	}
+	 //코너에서 전방까지의 벽
+	if (!isOutline)
+		return;
+	if (in_line + num >= 0)
+	{
+		for (int i = in_line + num; i < MAP_SIZE - in_line - num; i++)
+			board[i][in_line + num] = Void;
+	}
+}
+
+int GetNearestPlayerForward(const vector<vector<int>> &playerMap)
+{
+	int playerPos = GetPlayerPoston(playerMap);
+	for (int i = playerPos; playerPos > 0; i--)
+	{
+		if (playerMap[i][1] == 1)
+			return i;
+	}
+	return 0;
+}
+
+
+
+void SetStraight(vector<vector<string>>& displayBoard, vector<vector<int>> playerMap)
+{
+	int in_line = 0;
+	for (int i = GetPlayerPoston(playerMap); i > 0; i--)
+	{
+		if (playerMap[i][1] != 1)
+			in_line++;
+	}
 	in_line *= MOVE_STEP_SIZE;
 	SetWallInBoard(displayBoard);
-	// 각 모서리
+
+	// 전방 벽
+	int forward = GetNearestPlayerForward(playerMap);
+	for (int i = in_line; i < MAP_SIZE - in_line; i++)
+	{
+		// 상
+		displayBoard[in_line][i] = Void;
+		// 하
+		displayBoard[MAP_SIZE - in_line - 1][i] = Void;
+		// 좌
+		if (playerMap[forward][0] == 0)
+			displayBoard[i][in_line] = Void;
+		// 우
+		displayBoard[i][MAP_SIZE - in_line - 1] = Void;
+	}
+
+	// 왼쪽 모서리 선 모두 그리기
 	for (int i = 0; i < in_line; i++)
 	{
-		if (i < in_line - num || i >= in_line + num)
-		{
-			displayBoard[i][i] = Void;
-			displayBoard[MAP_SIZE - i - 1][i] = Void;
-		}
+		displayBoard[i][i] = Void;
+		displayBoard[MAP_SIZE - i - 1][i] = Void;
 		displayBoard[MAP_SIZE - i - 1][MAP_SIZE - i - 1] = Void;
 		displayBoard[i][MAP_SIZE - i - 1] = Void;
 	}
+}
 
-	// 왼쪽 벽
-	if (in_line - num >= 0)
+int GetZeroSize(const vector<vector<int>>& playerMap, int st)
+{
+	int cnt = 1;
+	for (int i = 1; i < playerMap.size(); i++)
 	{
-		for (int i = in_line - num; i < MAP_SIZE - in_line + num; i++)
-		{
-			displayBoard[i][in_line - num] = Void;
-		}
-	}
-
-	// 전방 벽
-	for (int i = in_line; i < MAP_SIZE - in_line; i++)
-	{
-		displayBoard[in_line][i] = Void;
-		displayBoard[MAP_SIZE - in_line - 1][i] = Void;
-		displayBoard[i][in_line] = Void;
-		displayBoard[i][MAP_SIZE - in_line - 1] = Void;
-	}
-	bool isWall = false;
-
-	// 해당 길로 갈때 (벽과 길이 닿아있을때)
-	if (isWall)
-	{
-		for (int i = in_line + 1; i < MAP_SIZE - in_line - 1; i++)
-			displayBoard[i][in_line] = Wall;
-
-		for (int i = 1; i <= num; i++)
-		{
-			if (in_line - i == 38)
-			{
-				displayBoard[in_line - 4][in_line - 5] = "  ";
-				displayBoard[in_line - 3][in_line - 5] = "  ";
-			}
-			if (in_line - i > MAP_SIZE / 2)
-				break;
-			displayBoard[in_line][in_line - i] = Void;
-			displayBoard[MAP_SIZE - in_line - 1][in_line - i] = Void;
-		}
-	}
-	else
-	{
-		// 전방 벽과 왼쪽 통로간의 가름선을 지움
-		//for (int i = in_line + 1; i < MAP_SIZE - in_line - 1; i++)
-		//	displayBoard[i][in_line] = Wall;
-
-		for (int i = 1; i <= num; i++)
-		{
-			if (in_line - i < 0)
-				continue;
-			// 디테일 살리기용 엄청 멀어졌을때 전방벽과, 왼쪽 벽을 이어주는 가로 선
-			if (in_line - i == MAP_SIZE / 2 + 2)
-			{
-				displayBoard[in_line - 4][in_line - 5] = "  ";
-				displayBoard[in_line - 3][in_line - 5] = "  ";
-			}
-			if (in_line - i > MAP_SIZE / 2)
-				break;
-			// 전방 벽에서 왼쪽 통로로 이어지는 것처럼보이게하는 가로 선들
-			displayBoard[in_line][in_line - i] = Void;
-			displayBoard[MAP_SIZE - in_line - 1][in_line - i] = Void;
-		}
+		if (playerMap[st - i][0] == 0)
+			cnt++;
+		else
+			return cnt;
 	}
 }
+
+// 왼쪽 코너가 벽과 붙어있지 않은상황
+// in_line : 왼쪽위부터 전방 벽까지의 거리인듯?
+void SetOutLineLeft(vector<vector<string>>& displayBoard, const vector<vector<int>>& playerMap)
+{
+	int playerPos = GetPlayerPoston(playerMap);
+	for (int i = playerPos; i > 0; i--)
+	{
+		if (playerMap[i][1] == 1)
+			break;
+		if (playerMap[i][0] == 0)
+		{
+			int leftPathCnt = GetZeroSize(playerMap, i);
+			SetLeftEdgeWall(displayBoard, (playerPos - i) * MOVE_STEP_SIZE, MOVE_STEP_SIZE * leftPathCnt);
+			if (playerMap[i - leftPathCnt - 1][1] == 0)
+				SetLeftVerticalWall(displayBoard, (playerPos - i) * MOVE_STEP_SIZE, MOVE_STEP_SIZE * leftPathCnt, true);
+			else
+				SetLeftVerticalWall(displayBoard, (playerPos - i) * MOVE_STEP_SIZE, MOVE_STEP_SIZE * leftPathCnt, false);
+			SetLeftHorizontalWall(displayBoard, (playerPos - i) * MOVE_STEP_SIZE, MOVE_STEP_SIZE * leftPathCnt);
+
+			i -= leftPathCnt;
+		}
+
+	}
+}
+
 
 void DisplayMap(vector<vector<int>> map) 
 {
@@ -156,11 +221,8 @@ void DisplayMap(vector<vector<int>> map)
 	}
 }
 
-int main()
+void InitMap(vector<vector<int>> &map)
 {
-	srand(time(NULL));
-	// 모두 꽉채우기(전체가 벽)
-	vector<vector<int>> map(18);
 	for (int i = 0; i < map.size(); i++)
 	{
 		map[i].push_back(1);
@@ -173,34 +235,50 @@ int main()
 	map[1][1] = 1;
 	map[2][0] = 0;
 	map[3][0] = 0;
+	map[8][0] = 0;
+	map[9][0] = 0;
 	map[map.size() - 1][1] = 1;
 	map[map.size() - 2][1] = 1;
-	vector<vector<string>> displayMap(MAP_SIZE);
+}
+
+void InitDisplayMap(vector<vector<string>>& map)
+{
 	for (int i = 0; i < MAP_SIZE; i++)
 	{
 		for (int j = 0; j < MAP_SIZE; j++)
-			displayMap[i].push_back(Wall);
-		displayMap[i].push_back("\n");
+			map[i].push_back(Wall);
+		map[i].push_back("\n");
 	}
-	int currPos = 4;
+}
+
+void SetDisplayMap(vector<vector<string>>& map, vector<vector<int>> playerMap)
+{
+	SetStraight(map, playerMap);
+	SetOutLineLeft(map, playerMap);
+}
+
+int main()
+{
+	srand(time(NULL));
+	// 모두 꽉채우기(전체가 벽)
+	vector<vector<int>> map(20);
+	InitMap(map);
+	vector<vector<string>> displayMap(MAP_SIZE);
+	InitDisplayMap(displayMap);
+
+	int currPos = 12;
 	map[currPos][1] = 2;
 	DisplayMap(map);
-	SetOutLineLeft(displayMap, currPos, PATH_SIZE, map);
-	//SetOutLine(displayMap, currPos * 2);
+	SetDisplayMap(displayMap, map);
 	DisplayBoard(displayMap);
 	while (1)
 	{
-		Sleep(50);
+		Sleep(30);
 		cout << currPos << endl;
 		char c = _getch();
 		if (c == 'w')
 		{
-			// SetOutLineLeft 에서 2 이하로 내려가면 에러
-			// 해당 구역은 반드시 벽이긴함
-			// 2로 잡은 이유는 랜덤배열 설정하고, 이를 두배로 복사 해야함
-			// 움직임을 더 표현할 수 있음(한번에 점프하는게 아니라 천천히 움직이게끔)
-			cout << currPos << "\n";
-			if (/*map[currPos - 1][1] != 1 && */currPos > 1)
+			if (map[currPos - 1][1] != 1 && currPos > 0)
 			{
 				map[currPos--][1] = 0;
 				map[currPos][1] = 2;
@@ -219,10 +297,9 @@ int main()
 			exit(0);
 		
 		system("cls");
+		SetDisplayMap(displayMap, map);
 		DisplayMap(map);
-		// 앞 뒤만 뚫려있음
-		SetOutLineLeft(displayMap, currPos, PATH_SIZE, map);
-		//SetOutLine(displayMap, currPos * 2);
+
 		DisplayBoard(displayMap);
 	}
 	//GameManager::GetInstance().GameStart();
